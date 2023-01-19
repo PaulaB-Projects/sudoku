f67495ab3c4088dcc95dcf727daca1fcfa9befee
Paula Burghelea <paulaburghelea.github@gmail.com>
2023-01-19 18:22:06 +0200

    added more code, up until min 1:44 of the recording

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {log as print} from 'node:console';
+import {count, log as print} from 'node:console';
 import {readFile as read} from 'node:fs/promises';
 
 const ENCODING = 'utf8';
@@ -16,7 +16,7 @@ const puzzle_to_solve = await read_data('./puzzle.json');
 const puzzle_solution = await read_data('./puzzle-solutions.json');
 
 class Grid extends Array {
-	constructor(grid) {
+	constructor(grid = [[]]) {
 		super(...grid);
 	}
 
@@ -41,14 +41,61 @@ const grid = new Grid(puzzle_to_solve);
 // print(grid.get_col(0));
 
 class Sudoku extends Grid {
-	#reference = [[]];
+	#reference = new Grid;
 
 	constructor(puzzle = [[]]) {
 		super([...puzzle.map(row => [...row])]);
-		this.#reference = puzzle;
+		this.#reference = new Grid(puzzle);
 
 		this.show();
 
+	}
+
+	guess(row = 0, col = 0, num = 1) {
+		if (!this.can_guess(row, col))
+			return false;
+
+		this.set(row, col, num);
+
+		if(!this.valid(row, col))
+			return false;
+		
+	}
+
+	can_guess(row = 0, col = 0, num = 1) {
+		return this.#reference.get(row, col) ===0;
+	}
+
+	is_valid(row = 0, col = 0) {
+		return check_row(row) &&
+					check_col(col) &&
+					check_box(row, col);
+	}
+
+	check_row(row = 0) {
+		return Sudoku.check_num_count(
+			this.row(row)
+		);
+	}
+
+	check_col(col = 0) {
+		return Sudoku.check_num_count(
+			this.col(col)
+		);
+	}
+
+	check_box(row = 0, col= 0) {
+		const [start_row, start_col] = [row, col].map(Sudoku.get_box_index);
+		const [end_row, end_col] = [start_row, start_row].map(coord =>
+			coord + Sudoku.BOX_SIZE);
+		
+		const box_as_array = [];
+
+		for (let row = start_row; row < end_row; row++)
+			for(let col = start_col; col < end_col; col++)
+			box_as_array.push(this.get(box_as_array));
+		
+		return Sudoku.check_num_count(box_as_array);
 	}
 
 	show() {
@@ -59,7 +106,23 @@ class Sudoku extends Grid {
 			row => row.map(
 				num => ` ${num} `
 			).join(PIPE)
-		).join(NEWLINE + LINE.repeat(4 * SUDOKU_LIMIT) + NEWLINE);
+		).join(NEWLINE + LINE.repeat(4 * SUDOKU_LIMIT - 1) + NEWLINE);
+	}
+
+	static BOX_SIZE = 3;
+	static get_box_index(coord) {
+		return coord - (coord % Sudoku.BOX_SIZE);
+	}
+
+	static check_num_count(array = []) {
+		const counts = new Array(9).fill(0);
+
+		array
+			.filter(num => num > 0)
+			.forEach(num => counts[num - 1] += 1
+);
+
+		return counts.every(count => count <= 1);
 	}
 }
 
